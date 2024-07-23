08891d62447eefb077bc844d7edea56b86930bf3
varscript <fvargas.edu@gmail.com>
2024-07-22 21:35:35 -0500

    get hero by url

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+  private hero?: Hero;
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id)))
+      .subscribe((hero) => {
+        if (!hero) this.router.navigate(['/heroes/list']);
+        this.hero = hero;
+        console.log(hero);
+
+        return;
+      });
+  }
 }
