1485b5295668309fe4cf3fa428760db1224a25e6
varscript <fvargas.edu@gmail.com>
2024-07-30 22:19:33 -0500

    CRUD endpoints

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,8 @@
+import { ActivatedRoute } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/heroes.interface';
 import { environments } from '../../../environments/environments';
 
@@ -22,8 +23,22 @@ export class HeroesService {
   }
 
   getSugessions(query: string): Observable<Hero[]> {
-    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`)
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
   }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.put<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
 
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw new Error('Hero id is required');
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHeroById(id: string): Observable<boolean> {
+    return this.http.delete(`${this.baseUrl}/heroes/${id}`).pipe(
+      catchError((err) => of(false)),
+      map((resp) => true)
+    );
+  }
 }
