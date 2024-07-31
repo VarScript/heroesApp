cfba0bdf8ba26b7630c692f860622d3c28a10c4d
varscript <fvargas.edu@gmail.com>
2024-07-30 22:51:49 -0500

    conectar el formulario con el template

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -8,19 +8,25 @@ import { Publisher } from '../../interfaces/heroes.interface';
   styles: ``,
 })
 export class NewPageComponent {
-
   public heroForm = new FormGroup({
-    id:               new FormControl<string>(''),
-    superhero:        new FormControl<string>('', { nonNullable: true}),
-    publisher:        new FormControl<Publisher>( Publisher.DCComics ),
-    alter_ego:        new FormControl(''),
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
     first_appearance: new FormControl(''),
-    characters:       new FormControl(''),
-    alt_img:          new FormControl(''),
-  })
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
 
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marver Comics', desc: 'Marver - Comics' },
   ];
+
+  onSubmit(): void {
+    console.log({
+      fromIsValid: this.heroForm.valid,
+      value: this.heroForm.value,
+    });
+  }
 }
