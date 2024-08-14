85d7667d645474e7743f93e8ea7da6bb134373ad
varscript <fvargas.edu@gmail.com>
2024-08-13 22:43:13 -0500

    mantener la sesion del usaurio

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, tap, map, catchError, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class Authservice {
@@ -23,8 +23,20 @@ export class Authservice {
     );
   }
 
+  checkAuthentication(): Observable<boolean> | boolean {
+    if (!localStorage.getItem('token')) return false;
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`).pipe(
+      tap((user) => (this.user = user)),
+      map((user) => !!user),
+      catchError((err) => of(false))
+    );
+  }
+
   logout(): void {
     this.user = undefined;
-    localStorage.clear()
+    localStorage.clear();
   }
 }
